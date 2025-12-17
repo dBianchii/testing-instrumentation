e8afdc9de78408a2e8b82b299f2617398d17165c
Gabriel Bianchi <gdbianchii@gmail.com>
2025-12-17 11:30:06 -0300

    ok

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,14 +1,19 @@
 import { Suspense } from "react";
 
 const getData = async () => {
-	"use cache";
+	"use cache: private";
 	return Math.random();
 }
 
 
 const InnerComponent = async () => {
-	const data = await getData();
-	return <p>Data: {data}</p>;
+
+	return <div>
+		<p>Data1: {await getData()}</p>
+		<p>Data2: {await getData()}</p>
+		<p>Data3: {await getData()}</p>
+		<p>Data4: {await getData()}</p>
+	</div>
 }
 
 export default function TestPage() {
